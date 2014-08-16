64c9b49c4b1129ba1fe511761d283c67b44e0049
Allen Sarkisyan <programminglinguist@gmail.com>
2014-08-16 04:18:05 -0700

    init

--- FirstMySQL/main.cpp
+++ FirstMySQL/main.cpp
@@ -6,13 +6,39 @@
 //  Copyright (c) 2014 Allen Sarkisyan. All rights reserved.
 //
 
+
 #include <iostream>
+#include <mysql.h>
+#include <my_global.h>
 
 int main(int argc, const char * argv[])
 {
-
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    MYSQL *con = mysql_init(NULL);
+    
+    if (con == NULL)
+    {
+        fprintf(stderr, "%s\n", mysql_error(con));
+        exit(1);
+    }
+    
+    if (mysql_real_connect(con, "localhost", "root", "relativity300",
+                           NULL, 0, NULL, 0) == NULL)
+    {
+        fprintf(stderr, "%s\n", mysql_error(con));
+        mysql_close(con);
+        exit(1);
+    }
+    
+    if (mysql_query(con, "CREATE DATABASE testdb"))
+    {
+        fprintf(stderr, "%s\n", mysql_error(con));
+        mysql_close(con);
+        exit(1);
+    }
+    
+    mysql_close(con);
+    
+    
     return 0;
 }
 
