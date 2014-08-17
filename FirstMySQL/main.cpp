302c9390d7949e413eb65bcacbf6b39fc2ce31f3
Allen Sarkisyan <programminglinguist@gmail.com>
2014-08-16 19:39:08 -0700

    super careful C++/MySQL C API test completed.

--- FirstMySQL/main.cpp
+++ FirstMySQL/main.cpp
@@ -10,10 +10,21 @@
 #include <iostream>
 #include <mysql.h>
 #include <my_global.h>
+#include <time.h>
+#include <stdlib.h>
+#include <sstream>
+
+void finish_with_error(MYSQL *con)
+{
+    fprintf(stderr, "%s\n", mysql_error(con));
+    mysql_close(con);
+    exit(1);
+}
 
 int main(int argc, const char * argv[])
 {
     MYSQL *con = mysql_init(NULL);
+    srand(time(NULL));
     
     if (con == NULL)
     {
@@ -21,7 +32,7 @@ int main(int argc, const char * argv[])
         exit(1);
     }
     
-    if (mysql_real_connect(con, "localhost", "root", "relativity300",
+    if (mysql_real_connect(con, "localhost", "root", "rootpass",
                            NULL, 0, NULL, 0) == NULL)
     {
         fprintf(stderr, "%s\n", mysql_error(con));
@@ -29,12 +40,47 @@ int main(int argc, const char * argv[])
         exit(1);
     }
     
-    if (mysql_query(con, "CREATE DATABASE testdb"))
-    {
+    if(mysql_query(con, "DROP DATABASE IF EXISTS test")){
         fprintf(stderr, "%s\n", mysql_error(con));
         mysql_close(con);
         exit(1);
     }
+    
+    if(mysql_query(con, "CREATE DATABASE IF NOT EXISTS test")){
+        fprintf(stderr, "%s\n", mysql_error(con));
+        mysql_close(con);
+        exit(1);
+    }
+    
+    if(mysql_query(con, "USE test")){
+        fprintf(stderr, "%s\n", mysql_error(con));
+        mysql_close(con);
+        exit(1);
+    }
+    
+    if(mysql_query(con, "CREATE TABLE IF NOT EXISTS testtable(id INT NOT NULL, nums INT,PRIMARY KEY(ID));")){
+        fprintf(stderr, "%s\n", mysql_error(con));
+        mysql_close(con);
+        exit(1);
+    }
+    
+    
+    for(int i=1;i<100;i++){
+        std::stringstream ss;
+        ss << "INSERT INTO testtable(id,nums) VALUES (" << i << "," << rand()%100 << ");";
+        if(mysql_query(con, ss.str().c_str())){
+            fprintf(stderr, "%s\n", mysql_error(con));
+            mysql_close(con);
+            exit(1);
+        }
+    }
+    
+    MYSQL_RES *result;
+    mysql_query(con, "SELECT nums FROM testtable WHERE id=4;");
+    result = mysql_store_result(con);
+    MYSQL_ROW row = mysql_fetch_row(result);
+    
+    std::cout<< *row << std::endl;
     
     mysql_close(con);
     
